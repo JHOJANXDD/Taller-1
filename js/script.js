f1a583436f57c562d1ec49500835e27fee5d83b4
kvelasco158 <kyvelasco04@misena.edu.co>
2022-08-04 11:37:30 -0500

    :)

---
+++ js/script.js
@@ -0,0 +1,93 @@
+console.log("UwU?");
+
+/*let numero=2315;
+
+console.log("El numero es",numero);
+console.log(`El numero es ${numero}`);
+
+let nombre="Nel";
+
+console.log(`El nombre es ${nombre}`)
+
+let boolean=false;
+
+console.log(`Esta ${boolean}`)
+
+let boolean2=true;
+
+console.log(`Esta ${boolean2}`)
+
+/*let res=numero+5;
+
+console.log(`El resultado es`+res);
+console.log(`El resultado es ${res}`);
+console.log(`El resultaod es ${numero+5}`);
+
+let estado=window.confirm("estas seguro de que akfskfsñaslkfñalkfpñ");
+console.log(`la ventana arrojo ${estado}`);
+
+let num1=prompt("digite un numero");
+let res=numero+parseInt(num1);
+console.log(`el resultado ${res}`);
+
+let num2=prompt("Digite un numero");
+let num3=prompt("Digite un numero");
+let sum=parseInt(num2)+parseInt(num3);
+console.log(`El resultado es ${res}`);
+
+//Condicionales 
+if(num2>num3){
+    console.log(`El ${num2} es mayor que ${num3}`)
+}else if(num2==num3){
+    console.log(`Los numeros son iguales`)
+
+}else{
+    console.log(`El ${num2  } es mayor que ${num3}`)
+};
+
+/*Operador
+let nummayot=(num1>num2)
+?"El numero mayor es el primero"
+:"El numero mayor es el segundo"
+
+console.log(nummayor);
+
+
+
+//Condicionales con condicion compuesta
+let usuario=prompt("Digite su usuario");
+let contraseña=prompt("Digite su contraseña");
+
+if(usuario=="Johan" && contraseña=="hola"){
+    window.alert("bienvenido")
+}else if(usuario=="johan" || contraseña=="jola"){
+    window.alert("Verifique su contraseña")
+}else if(usuario=="ojan" || contraseña=="hola"){
+    window.alert("Verifique su usuario")
+}else(
+    window.alert("Credenciales Incorrectas")    
+)
+*/
+//
+let num1=parseInt(prompt("Ingrese el Primer numero")); 
+let num2=parseInt(prompt("Ingrese el Segundo numero")); 
+    let operacion=prompt("Digite la operacion que quiere realizar M=Multiplicar "); 
+    switch(operacion){ 
+    case 'M': 
+    console.log(`el resultado es ${num1*num2} `); 
+    break; 
+    case 'S': 
+    console.log(`el resultado es ${num1+num2}`); 
+    break; 
+    case 'R': 
+    console.log(`el resultado es ${num1-num2}`); 
+    break; 
+    case 'D': 
+    console.log(`el resultado es ${num1/num2}`); 
+    break;
+    case 'P':
+    console.log(`el resultado es ${math.pow(num1, 2)}`);
+    break;
+    case 'F':
+    console.log(`el resultado es ${math.squirt(num1)}`);
+}
